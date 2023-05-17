4b9b23ab90cc860f1070a7e4ffea8889a1975e4c
Francesco Ryu <francescoryu04@gmail.com>
2023-05-17 14:19:17 +0200

    Changes by gabri

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,14 @@ from controller.auth import blueprint
 
 app = Flask(__name__)
 
-app.add_url_rule("/api/auth/login", "login", auth_func=auth.login)
+app.register_blueprint(blueprint, url_prefix="/auth")
+
+
+#app.add_url_rule("/api/auth/login", "login", auth.login)
+
+@app.route('/')
+def test():
+    return connect_database("SELECT * FROM User")
 
 
 if __name__ == '__main__':
