dab64f4feebc2da8dc693cea4599368cb280109f
ysave <nick.c@menisch.ch>
2023-05-24 14:12:09 +0200

    added controller

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios, { AxiosResponse } from 'axios';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  onClicksubmit(data: any ){
+    console.log(data);
+    const inputs = [data.username, data.password]
+    axios
+      .post('http://localhost:5000/auth/login', inputs)
+      .then((response: AxiosResponse) => {
+        console.log("your logged In")
+    })
+      .catch((error: any) => {
+        console.log(error)
+      })
+    }
 }
