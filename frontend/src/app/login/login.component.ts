ba0c93db754e49b94478515601156ec7eedd0cfc
ysave <nick.c@menisch.ch>
2023-05-24 15:06:27 +0200

    login controller working

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import axios, { AxiosResponse } from 'axios';
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-login',
@@ -8,10 +13,13 @@ import axios, { AxiosResponse } from 'axios';
 })
 export class LoginComponent {
   onClicksubmit(data: any ){
+    const formData = new FormData();
+    formData.append("username",data.username);
+    formData.append("password",data.password);
+
     console.log(data);
-    const inputs = [data.username, data.password]
     axios
-      .post('http://localhost:5000/auth/login', inputs)
+      .post('http://localhost:5000/auth/login', formData)
       .then((response: AxiosResponse) => {
         console.log("your logged In")
     })
