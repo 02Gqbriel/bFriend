df1b5153f694c2683a896d14d30d2dafd4f911af
Francesco Ryu <francescoryu04@gmail.com>
2023-05-24 11:22:07 +0200

    convertable to json

--- backend/connect.py
+++ backend/connect.py
@@ -1,28 +1,38 @@
 import sqlite3
+import json
 
 
 def connect_database(prep_stmt, *args):
     sql_conn = sqlite3.connect('../database/sqlite.db')
     cursor = sql_conn.cursor()
-    # only prep_stmt required e.g: connect_database("SELECT * ...", None)
+
     if args:
-        try:
-            cursor.execute(prep_stmt)
-            sql_conn.commit()
-            response = cursor.fetchall()
-            sql_conn.close()
-            return response
-        except sqlite3.Error as error:
-            sql_conn.rollback()
-            return str(error)
-    # prep_stmt AND values required e.g: connect_database("SELECT * ... WHERE column1 = ?...", value1, ...)
-    else:
         try:
             cursor.execute(prep_stmt, args)
             sql_conn.commit()
             response = cursor.fetchall()
             sql_conn.close()
-            return response
+            if len(response) == 1:
+                return json.dumps(response[0])
+            else:
+                return json.dumps(response)
+        except sqlite3.Error as error:
+            sql_conn.rollback()
+            return str(error)
+    else:
+        try:
+            cursor.execute(prep_stmt)
+            sql_conn.commit()
+            response = cursor.fetchall()
+            result = [
+                dict(zip(["ID", "Username", "Password", "First Name", "Last Name", "Age", "Hobby", "Status"], row)) for
+                row in response]
+            json_data = json.dumps(result)
+            sql_conn.close()
+            if len(response) == 1:
+                return json.dumps(response[0])
+            else:
+                return json_data
         except sqlite3.Error as error:
             sql_conn.rollback()
             return str(error)
