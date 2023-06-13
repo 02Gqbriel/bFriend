7f5b8ae5413b5d758e653b6ff14f9d6e1812655e
ysave <nick.c@menisch.ch>
2023-06-13 13:54:37 +0200

    register working :(

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import axios, {AxiosResponse} from "axios";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  onClickSubmit(data: any ){
+    const formData = new FormData();
+    formData.append("username",data.username);
+    formData.append("password",data.password);
+
+    axios
+      .post('http://localhost:5000/auth/register', formData)
+      .then((response: AxiosResponse) => {
+        console.log("registration succesfully")
+    })
+      .catch((error: any) => {
+        console.log(error)
+      })
+    }
 }
