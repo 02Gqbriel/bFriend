4c0459dc8eb34d17802cc1ae99f3a84aa5b4a2db
save <nick.c@menisch.ch>
2023-06-13 16:31:38 +0200

    register with first,last,username,pw

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -12,7 +12,9 @@ export class RegisterComponent {
     const formData = new FormData();
     formData.append("username",data.username);
     formData.append("password",data.password);
-
+    formData.append("firstname",data.firstname);
+    formData.append("lastname",data.lastname);
+    console.log(formData)
     axios
       .post('http://localhost:5000/auth/register', formData)
       .then((response: AxiosResponse) => {
