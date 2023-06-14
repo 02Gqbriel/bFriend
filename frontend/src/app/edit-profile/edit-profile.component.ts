884a4d0390867e2791d266276ccd31334735fd9d
fftop-dev <feroldif@bzz.ch>
2023-06-14 11:07:05 +0200

    edit user and nav bar

--- frontend/src/app/edit-profile/edit-profile.component.ts
+++ frontend/src/app/edit-profile/edit-profile.component.ts
@@ -10,13 +10,18 @@ export class EditProfileComponent {
   edit_profile_onClick(data: any ){
     if (data.password == data.password_confirm) {
       const formData = new FormData();
+      
+      formData.append("firstname", data.firstname);
+      formData.append("firstname", data.lastname);
+      formData.append("firstname", data.age);
       formData.append("username", data.username);
+      formData.append("email", data.email);
       formData.append("password", data.password);
       formData.append("password_confirm", data.password_confirm);
 
       console.log(formData);
       axios
-        .post('http://localhost:5000/update/user', formData)
+        .post('http://localhost:5000/auth/update', formData)
         .then((response: AxiosResponse) => {
           console.log("user data has been submitted")
         })
@@ -27,5 +32,16 @@ export class EditProfileComponent {
     else{
       console.log("password does not match")
     }
+  }
+  get_user_session(){
+    axios
+      .get("http://localhost:5000/auth/update")
+              .then((response: AxiosResponse) => {
+          console.log("user data has been received")
+                return response.data;
+        })
+        .catch((error: any) => {
+          console.log(error)
+        })
   }
 }
