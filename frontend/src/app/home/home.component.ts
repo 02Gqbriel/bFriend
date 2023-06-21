34e4238a7f7c267b3fe2d498d880a67415e6e454
Nimai Leuenberger <leuenbergern2@bzz.ch>
2023-06-21 13:34:07 +0200

    homepage updated

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import axios, {AxiosResponse} from "axios";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  userData: any[] = [];
+  i: number = 0;
 
+  constructor() {
+    this.renderHomepage()
+  }
+  renderHomepage () {
+    axios
+      .post('http://localhost:5000/user_actions/get-all')
+      .then((response: AxiosResponse) => {
+        this.userData = response.data
+        console.log(this.userData)
+    })
+      .catch((error: any) => {
+        console.log(error)
+      })
+    }
+
+  ngOnInit(): void {
+
+  }
+
+  btnClick() {
+    this.i = this.i + 1;
+
+  }
 }
