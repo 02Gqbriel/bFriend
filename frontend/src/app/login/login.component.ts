f956789bc5ed8f2465e7406d10fa3271b7ed3a60
ysave <nick.c@menisch.ch>
2023-06-21 13:45:39 +0200

    login redirect

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import axios, { AxiosResponse } from 'axios';
 import {Injectable} from "@angular/core";
+import {Router} from "@angular/router"
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,7 @@ import {Injectable} from "@angular/core";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) { }
   onClickSubmit(data: any ){
     const formData = new FormData();
     formData.append("username",data.username);
@@ -22,6 +25,8 @@ export class LoginComponent {
       .post('http://localhost:5000/auth/login', formData)
       .then((response: AxiosResponse) => {
         console.log("your logged In")
+        console.log(response.data)
+        this.router.navigate(['/'])
     })
       .catch((error: any) => {
         console.log(error)
