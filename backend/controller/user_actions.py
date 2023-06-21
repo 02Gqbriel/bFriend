6ad7e6f66b464809a93a142130414482efacd9b8
Francesco Ryu <francescoryu04@gmail.com>
2023-06-21 09:31:02 +0200

    get 1 user

--- backend/controller/user_actions.py
+++ backend/controller/user_actions.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from backend.model.user_dao import select_all
+from backend.model.user_dao import select_all, select_user
 
 blueprint = Blueprint("auth", __name__)
 
@@ -8,3 +8,11 @@ blueprint = Blueprint("auth", __name__)
 def get_all_users():
     all_users = select_all()
     return all_users
+
+
+@Blueprint.route("/select-user", methods=["POST"])
+def select_user():
+    user_id = request.form["userID"]
+    user = select_user(int(user_id))
+    return user
+
