0c0a3f3c3388d36e235c11e4ea25e83296a6aa41
ysave <nick.c@menisch.ch>
2023-06-21 11:28:27 +0200

    added settings view

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios, {AxiosResponse} from "axios";
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
-
+  deleteAccount(){
+    const formData = new FormData()
+    formData.append("userID", "1");
+    axios
+      .post('http://localhost:5000/user_actions/delete-user', formData)
+      .then((response: AxiosResponse) => {
+        console.log("your logged In")
+        console.log(response)
+    })
+      .catch((error: any) => {
+        console.log(error)
+      })
+    }
 }
