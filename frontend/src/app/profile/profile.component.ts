f708da83b2140a77b6b6b5630c96b7bd18c552ec
Nimai Leuenberger <leuenbergern2@bzz.ch>
2023-06-21 13:42:02 +0200

    profile updated

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios, {AxiosResponse} from "axios";
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  userData: any[] = [];
+  i: number = 0;
 
+  constructor() {
+    this.renderProfile()
+  }
+  renderProfile () {
+    axios
+      .post('http://localhost:5000/user_actions/get-all')
+      .then((response: AxiosResponse) => {
+        this.userData = response.data
+        console.log(this.userData)
+    })
+      .catch((error: any) => {
+        console.log(error)
+      })
+    }
+
+  ngOnInit(): void {
+
+  }
+
+  btnClick() {
+    this.i = this.i + 1;
+
+  }
 }
